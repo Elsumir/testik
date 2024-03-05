a641542cd7e67bba4ba64ab8bf6ffb26f5850263
Serik Askaroff <serik.askaroff@gmail.com>
2024-03-05 15:52:35 +0300

    добавил ссылку с https

--- src/api/const.js
+++ src/api/const.js
@@ -5,4 +5,7 @@ const date = new Date().toISOString().slice(0, 10).split('-').join('');
 const data = `${password}_${date}`;
 
 export const token = md5(data).toString();
-export const URL_API = 'http://api.valantis.store:40000/';
+export const URL_API = 'https://api.valantis.store:41000';
+
+// https://api.valantis.store:41000/
+// http://api.valantis.store:40000/
